b8b498d1dc6fd9f9a85bbf782d972ccfd08ede03
Suleiman <rocklessg@gmail.com>
2025-03-29 15:18:52 +0100

    Implement chatClient sample usage and update release note

--- FireflyTester/Program.cs
+++ FireflyTester/Program.cs
@@ -7,11 +7,21 @@ using FireflyTester.WebSocketImplementationSamples;
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 Console.WriteLine("Testing Firefly HTTP Requests...\n");
 
+#region Http
+
 //await AvancedSampleUsage.RunTests();
 //await SampleDownloadFile.RunTests();
 //await SampleDeserializedResponse.RunTests();
 
-//Websocket flow
-await WebSocketSampleUsage.WithLogger();
-//await WebSocketSampleUsage.WithoutLogger(); // Uncomment to test without logger
-    
+#endregion
+
+#region Web Socket
+
+//await WebSocketSampleUsage.WithLogger();
+//await WebSocketSampleUsage.WithoutLogger(); // test without logger
+
+string wsUrl = "wss://echo.websocket.org"; // Use a test WebSocket server
+var chatClient = new ChatClient();
+await chatClient.StartAsync(wsUrl);
+
+#endregion
